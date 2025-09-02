cbd5e5376a155a9c534b687dc8f4c60ed6dc228e
nivla0021 <alvinabogado885@gmail.com>
2025-09-02 11:34:36 +0800

    seperate into smaller components

---
+++ src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,41 @@
+import { CartItemDetails } from './CartItemDetails'
+import { DeliveryDate } from './DeliveryDate'
+import { DeliveryOptions } from './DeliveryOptions'
+
+export function OrderSummary( {cart, deliveryOptions} ) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+
+        const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
+          return deliveryOption.id === cartItem.deliveryOptionId
+        })  
+
+        return (
+          <div key={cartItem.productId} className="cart-item-container">
+            <DeliveryDate 
+              selectedDeliveryOption={selectedDeliveryOption} 
+            />
+
+            <div className="cart-item-details-grid">
+              <img 
+                className="product-image"
+                src={cartItem.product.image} 
+              />
+
+              <CartItemDetails 
+                cartItem={cartItem}
+              />
+
+              <DeliveryOptions 
+                cartItem={cartItem} 
+                deliveryOptions={deliveryOptions} 
+              />
+            </div>
+          </div>
+        )
+      })}
+          
+    </div>
+  )
+}
